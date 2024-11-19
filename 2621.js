33b0be1fa9f75354f5fddca63e9a8708c2eeed77
Mateus Vilaverde <matvilaverde@casuldev.com>
2024-11-19 17:51:22 -0300

    add sleep function alternative

--- 2621.js
+++ 2621.js
@@ -43,3 +43,9 @@ async function sleep(millis) {
  * let t = Date.now()
  * sleep(100).then(() => console.log(Date.now() - t)) // 100
  */
+
+// Alternative
+
+// async function sleep(millis) {
+// 	return new Promise((resolve) => setTimeout(() => resolve(millis), millis));
+// }
